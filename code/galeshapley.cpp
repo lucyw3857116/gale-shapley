ff2e06b5b2d89cd92a39bf65a3a1832678847f79
Lucy Wang <lexinw@ghc29.ghc.andrew.cmu.edu>
2025-04-19 15:59:07 -0400

    included data generation in shared address space

--- code/galeshapley.cpp
+++ code/galeshapley.cpp
@@ -67,6 +67,36 @@ void find_stable_pairs(std::vector<Participant>& participants, int n, int numPre
 
 }
 
+bool is_stable_matching(const std::vector<Participant>&participants, int n) {
+    for (int m = 0; m < n; m++) {
+        int w = participants[m].current_partner_id;
+        const auto& m_prefs = participants[m].preferences;
+        const auto& w_prefs = participants[w].preferences;
+        for (int preferred_w : m_prefs) {
+            if (preferred_w == w) {
+                break; // found the current partner, no need to check further
+            }
+            int her_current = participants[preferred_w].current_partner_id;
+            const auto& her_prefs = participants[preferred_w].preferences;
+            int m_rank = -1;
+            int her_current_rank = -1;
+            for (int i = 0; i < her_prefs.size(); i++) {
+                if (her_prefs[i] == m) {
+                    m_rank = i;
+                }
+                if (her_prefs[i] == her_current) {
+                    her_current_rank = i;
+                }
+            }
+            if (m_rank < her_current_rank) {
+                // m prefers preferred_w over w, and preferred_w prefers m over her current partner
+                return false; // not stable
+            }
+        }
+    }
+    return true;
+}
+
 void find_stable_pairs_parallel(std::vector<Participant>& participants, int n, int numPreferences, int num_threads){
     // intialize everyone as free
     for (int i = 0; i < 2*n; i++) {
@@ -150,17 +180,18 @@ void find_stable_pairs_parallel(std::vector<Participant>& participants, int n, i
 int main (int argc, char *argv[]) {
     const auto init_start = std::chrono::steady_clock::now();
     std::string input_filename, mode;
-    int num_threads = 0;
+    int num_threads = 1;
     int opt;
-    while ((opt = getopt(argc, argv, "f:m:n:")) != -1) {
+    int num;
+    while ((opt = getopt(argc, argv, "m:n:t:")) != -1) {
         switch (opt) {
-            case 'f':
-                input_filename = optarg;
+            case 'n':
+                num = atoi(optarg);
                 break;
             case 'm':
                 mode = optarg;
                 break;
-            case 'n':
+            case 't':
                 num_threads = atoi(optarg);
                 break;
             default:
@@ -168,72 +199,39 @@ int main (int argc, char *argv[]) {
                 exit(EXIT_FAILURE);
         }
     }
-    // Check if required options are provided
-    if (empty(input_filename) || empty(mode) || num_threads <= 0) {
-        std::cerr << "Usage: " << argv[0] << " -f input_filename -n num_threads -m parallel_mode\n";
-        exit(EXIT_FAILURE);
-    }
-    std::cout << "Input file: " << input_filename << '\n';
-    std::ifstream fin(input_filename);
-    if (!fin) {
-        std::cerr << "Unable to open file: " << input_filename << ".\n";
-        exit(EXIT_FAILURE);
-    }
-    int num, preferenceNum;
-
-    // read the information about the data set
-    fin >> num;
+    
     std::vector<Participant> participants(num*2);
-    for (int i = 0; i < num*2; i++) {
+    
+    for (int i = 0; i < num * 2; i++) {
+        std::vector<int> prefs(num);
+        for (int j = 0; j < num; j++) {
+            prefs[j] = j;
+        }
+        std::mt19937 rng(i * 1000 + 42); // check this
+        std:shuffle(prefs.begin(), prefs.end(), rng);
         Participant p;
         p.id = i;
-        fin >> preferenceNum;
-        
-        
-        if (i >= num) { // is a woman
-            for (int j = 0; j < preferenceNum; j++) {
-                int preference;
-                fin >> preference;
-                p.preferences.push_back(preference-1);
+        if (i < num) {
+            for (int j = 0; j < num; j++) {
+                p.preferences.push_back(prefs[j] + num);
             }
-            // for (int idx = preferenceNum-1; idx >= 0; idx--) {
-            //     int man_id = p.preferences[idx];
-            //     p.preferenceRank[man_id] = idx;
-            // }
-            
         } else {
-            for (int j = 0; j < preferenceNum; j++) {
-                int preference;
-                fin >> preference;
-                p.preferences.push_back(preference+num-1);
+            for (int j = 0; j < num; j++) {
+                p.preferences.push_back(prefs[j]);
             }
         }
         participants[i] = p;
-        // std::cout << "id: " << p.id << ", preferences: ";
-        // for (int pref : p.preferences) {
-        //     std::cout << pref << " ";
-        // }
-        // std::cout << '\n';
     }
-    //print out participants
-    // std::cout << "Participants:\n";
-    // for (auto& p : participants) {
-    //     std::cout << "id: " << p.id << ", preferences: ";
-    //     for (int pref : p.preferences) {
-    //         std::cout << pref << " ";
-    //     }
-    //     std::cout << '\n';
-    // }
     const double init_time = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - init_start).count();
     std::cout << "Initialization time (sec): " << std::fixed << std::setprecision(15) << init_time << '\n';
     const auto compute_start = std::chrono::steady_clock::now();
 
     if (mode == "s") {
         std::cout << "Running serial code \n";
-        find_stable_pairs(participants, num, preferenceNum);
+        find_stable_pairs(participants, num, num);
     } else if (mode == "p1") {
         std::cout << "Running pii code \n";
-        find_stable_pairs_parallel(participants, num, preferenceNum, num_threads);
+        find_stable_pairs_parallel(participants, num, num, num_threads);
     } else if (mode == "p2") {
         std::cout << "Running pii-sc code \n";
         // find_stable_pairs_parallel_sc(participants, num, preferenceNum);
@@ -249,7 +247,7 @@ int main (int argc, char *argv[]) {
     if (std::size(input_filename) >= 4 && input_filename.substr(std::size(input_filename) - 4) == ".txt") {
         input_filename.resize(std::size(input_filename) - 4);
     }
-    const std::string output_filename = input_filename + "_output.txt";
+    const std::string output_filename = std::to_string(num) + "_output.txt";
 
     std::ofstream output(output_filename, std::fstream::out);
     if (!output) {
@@ -257,18 +255,12 @@ int main (int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     
-    // for (int i = 0; i < num; ++i) {
-    //     output << participants[i].id << " " << participants[i].current_partner_id << "\n";
-    // }
-    for (int i = 0; i < num; ++i) {
-        if (participants[i].current_partner_id != -1) {
-            output << participants[i].id << " " << participants[i].current_partner_id << "\n";
-        } else {
-            std::cerr << "Warning: participant " << i << " was not matched.\n";
-        }
+    // validation
+    bool valid;
+    valid = is_stable_matching(participants, num);
+    if (!valid) {
+        std::cerr << "Warning: the matching is not stable.\n";
+    } else {
+        std::cout << "The matching is stable.\n";
     }
-    
-    output.close();
-    std::cout << "Matches written to " << output_filename << "\n";
-
 }
